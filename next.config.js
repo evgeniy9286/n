8680d2c9ed2923a56ad72bd8a641dec54eed0286
Evgeniy Makhnin <61658755+evgeniy9286@users.noreply.github.com>
2022-07-14 18:17:03 +0200

    First commit

--- next.config.js
+++ next.config.js
@@ -4,4 +4,16 @@ const nextConfig = {
   swcMinify: true,
 }
 
+const STUDIO_REWRITE = {
+  source: "/studio/:path*",
+  destination:
+    process.env.NODE_ENV === "development"
+      ? "http://localhost:3333/studio/:path*"
+      : "/studio/index.html",
+};
+
 module.exports = nextConfig
+
+module.exports = {
+  rewrites: () => [STUDIO_REWRITE],
+};
